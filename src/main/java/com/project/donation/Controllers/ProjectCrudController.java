a381e5abdd7ef1c168ef0d53a4e0974c2de5f9fc
asus <a.naji9710@gmail.com>
2019-07-04 16:08:16 +0100

    first => Project crud

--- src/main/java/com/project/donation/Controllers/ProjectCrudController.java
+++ src/main/java/com/project/donation/Controllers/ProjectCrudController.java
@@ -1,6 +1,37 @@
 package com.project.donation.Controllers;
 
+import com.project.donation.Models.Project;
+import com.project.donation.Services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProjectCrudController {
 
+    @Autowired
+    private ProjectService projectService;
 
+    @GetMapping("/projects")
+    public List<Project> projects(){
+      return null;
+    }
+
+    @PostMapping("/newProject")
+    public Boolean createProject(){
+        return null;
+    }
+
+    @GetMapping("/updateProject")
+    public Boolean updateProject(){
+        return null;
+    }
+
+    @GetMapping("/deleteProect/{id}")
+    public Boolean deleteProject(){
+        return null;
+    }
 }
---
+++ src/main/java/com/project/donation/Services/ServicesImpl/ProjectServiceImpl.java
@@ -0,0 +1,11 @@
+package com.project.donation.Services.ServicesImpl;
+
+
+import com.project.donation.Services.ProjectService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+
+}
